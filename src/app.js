575fea5d9118a76871fd7c0f6b0d9e625883edae
Matt Heerspink <mheerspink75@gmail.com>
2021-07-12 01:11:52 -0500

    updated functions and added error handling

--- src/app.js
+++ src/app.js
@@ -36,41 +36,52 @@ const createTable = () => {
     </table>`;
 };
 
-// Fetch json data
-const fetchSymbols = async () => {
-  const res = await fetch(symbolsUrl);
-  const data = await res.json();
-
-  createTable();
-
-  // Update Table
-  for (const key of Object.keys(data.DISPLAY)) {
-    let key1 = data.DISPLAY[key].USD;
-
-    let id = document.querySelector("#table");
-    let newTr = document.createElement("tr");
-    id.appendChild(newTr);
-
-    let newTd = document.createElement("td");
-    newTr.appendChild(newTd);
-    newTd.innerHTML = key;
-
-    let newTd2 = document.createElement("td");
-    newTr.appendChild(newTd2);
-    newTd2.innerHTML = key1.PRICE;
-
-    let newTd3 = document.createElement("td");
-    newTr.appendChild(newTd3);
-    newTd3.innerHTML = key1.MKTCAP;
-
-    let newTd4 = document.createElement("td");
-    newTr.appendChild(newTd4);
-    newTd4.innerHTML = key1.CHANGEPCT24HOUR + " %";
-
-    let newTd5 = document.createElement("td");
-    newTr.appendChild(newTd5);
-    newTd5.innerHTML = key1.CHANGE24HOUR;
+// Fetch data
+const getData = async () => {
+  const response = await fetch(symbolsUrl);
+  if (response.status !== 200) {
+    throw new Error();
   }
+  const data = await response.json();
+  return data;
 };
 
-fetchSymbols();
+// Update table with API data
+getData()
+  .then((data) => {
+    console.log("Success!", data);
+
+    createTable();
+    // Update Table
+    for (const key of Object.keys(data.DISPLAY)) {
+      let key1 = data.DISPLAY[key].USD;
+
+      let id = document.querySelector("#table");
+      let newTr = document.createElement("tr");
+      id.appendChild(newTr);
+
+      let newTd = document.createElement("td");
+      newTr.appendChild(newTd);
+      newTd.innerHTML = key;
+
+      let newTd2 = document.createElement("td");
+      newTr.appendChild(newTd2);
+      newTd2.innerHTML = key1.PRICE;
+
+      let newTd3 = document.createElement("td");
+      newTr.appendChild(newTd3);
+      newTd3.innerHTML = key1.MKTCAP;
+
+      let newTd4 = document.createElement("td");
+      newTr.appendChild(newTd4);
+      newTd4.innerHTML = key1.CHANGEPCT24HOUR + " %";
+
+      let newTd5 = document.createElement("td");
+      newTr.appendChild(newTd5);
+      newTd5.innerHTML = key1.CHANGE24HOUR;
+    }
+  })
+  .catch((err) => {
+    console.log("Error!", err.message);
+    document.getElementById("app").innerHTML = "Error!: " + err.message;
+  });
